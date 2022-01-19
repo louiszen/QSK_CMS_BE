d6147ab37e136b7b459e92e6b881a42321c0c367
Louiszen <louiszen@gmail.com>
2022-01-20 02:04:53 +0800

    SYS

--- __SYSDefault/SYSAuth.js
+++ __SYSDefault/SYSAuth.js
@@ -10,7 +10,14 @@
     Dashboard: ["Submit", "Approve"],
     System: {
       BnR: ["List", "Add", "Edit", "Delete"],
-      UAC: ["List", "Add", "Edit", "Delete"],
+      UAC: {
+        General: ["View"],
+        API: ["View", "Edit"],
+        AuthTree: ["View", "Edit"],
+        Roles: ["View", "Edit", "Add", "Delete"],
+        Groups: ["View", "Edit", "Add", "Delete"],
+        Users: ["View", "Edit", "Add", "Delete"]
+      },
       AuthLog: ["List"],
       Tickets: ["List", "Add", "Edit", "Delete"],
     },
