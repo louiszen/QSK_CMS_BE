d897e1f7a424073e039d52e3d7252add13b5aaf1
Louiszen <louiszen@gmail.com>
2022-01-20 23:47:11 +0800

    SYSAuth

--- __SYSDefault/SYSAuth.js
+++ __SYSDefault/SYSAuth.js
@@ -15,8 +15,10 @@
         API: ["View", "Edit"],
         AuthTree: ["View", "Edit"],
         Roles: ["View", "Terminate", "Edit", "Add", "Delete", "TreeView", "TreeEdit"],
-        Groups: ["View", "Terminate", "Edit", "Add", "Delete", "TreeView", "TreeEdit"],
-        Users: ["View", "Terminate", "Edit", "Add", "Delete", "TreeView", "TreeEdit"]
+        Groups: ["View", "Terminate", "Edit", "Add", "Delete", 
+          "UserView", "UserAdd", "UserEdit", "UserDelete"],
+        Users: ["View", "Terminate", "Edit", "Add", "Delete", 
+          "TreeView", "TreeEdit", "GroupView", "GroupAdd", "GroupEdit", "GroupDelete"]
       },
       AuthLog: ["List"],
       Tickets: ["List", "Add", "Edit", "Delete"],
